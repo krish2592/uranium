2b3b9a739a10226e145be4e90f4bb16cc2fffed5
krish2592 <sachink9225s@gmail.com>
2022-04-06 17:06:40 +0530

    adding new module logger.js

---
+++ src/routes/logger.js
@@ -0,0 +1,8 @@
+ let endpoint="www.google.com"
+
+ let name= function myName(){
+     return "Sachin Mishra"
+ }
+
+ module.exports.url=endpoint;
+ module.exports.name=name;
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,13 @@
-const express = require('express');
+const express = require(
+    'express');
+const log = require('./logger');
 
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
+    console.log("Imorting url from logger",log.url)
+    console.log("Hi, This is: ",log.name())
 });
 
 module.exports = router;
